2dd38b435ce87ab6e8186d4799e4c3321978c9cf
Tony Tan <t347100955@gmail.com>
2019-02-08 23:32:49 -0800

    Renamed project name.

--- app/src/main/java/com/triplec/triway/MainActivity.java
+++ app/src/main/java/com/triplec/triway/MainActivity.java
@@ -1,4 +1,4 @@
-package com.triplec.mytrip;
+package com.triplec.triway;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.triplec.triway.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
