aecc4db50d02d7f280b44421339fc0333515b6db
Tony Tan <t347100955@gmail.com>
2019-02-24 13:18:27 -0800

    Fixed bug on android 9.

--- app/src/main/java/com/triplec/triway/LoginActivity.java
+++ app/src/main/java/com/triplec/triway/LoginActivity.java
@@ -1,9 +1,9 @@
 package com.triplec.triway;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
@@ -13,17 +13,14 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-import com.triplec.triway.R;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class LoginActivity extends AppCompatActivity {
     private Button signUp, login;
